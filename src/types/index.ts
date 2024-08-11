7ec3923280a9975a5ad2bd16fda2ed94ba3570a9
JFmaia <jfmaia.dev@gmail.com>
2024-08-11 10:39:44 -0300

    feat: Created new type (#11)

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,3 +22,32 @@ export interface CreateLaboratory {
   email: string;
   image: string | null;
 }
+
+export interface Pending {
+  id: string | null;
+  id_user: string | null;
+  id_lab: string | null;
+  id_project: string | null;
+  ativo: boolean | null;
+  data_create: string | null;
+  data_atualizacao: string | null;
+  matricula_user: string | null;
+}
+
+export interface UsuarioResponse {
+  id: string;
+  data_inicial: string;
+  data_atualizacao: string;
+  primeiro_acesso: boolean;
+  ativo: boolean;
+  primeiro_nome: string;
+  image: string | null;
+  segundo_nome: string;
+  data_nascimento: string;
+  genero: string;
+  email: string;
+  matricula: number;
+  tel: number;
+  permissoes: Array<Permission>;
+  lista_pending: Array<Pending>;
+}
